c5427683efa9c6196a1fe18a674c4fa55c58c434
Tippins <ikp9@cdc.gov>
2026-08-10 10:48:15 -0400

    New run

--- install_packages.R
+++ install_packages.R
@@ -3,7 +3,7 @@ new <- packages[!packages %in% rownames(installed.packages())]
 if(length(new)) install.packages(new)
 
 
-#When new version shiny is needed:
+#Run this only new version shiny is needed:
 install.packages("shiny")
 library(shiny)
 packageVersion("shiny")
